d82f9d08b8bc5c28ede16e94d5cc0dcb82956fd5
aymensellaouti <aymen.sellaouti@gmail.com>
2025-09-15 13:43:08 +0100

    rotating card exercice

--- src/app/components/rotating-card/rotating-card.component.ts
+++ src/app/components/rotating-card/rotating-card.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-rotating-card',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './rotating-card.component.html',
-  styleUrl: './rotating-card.component.css'
+  styleUrl: './rotating-card.component.css',
 })
 export class RotatingCardComponent {
-
+  // Définir mon state => créer les attributs qui décrivent ma vue
+  name = 'sellaouti';
+  firstname = 'aymen';
+  job = 'Formateur';
+  path = 'rotating_card_profile3.png';
 }
