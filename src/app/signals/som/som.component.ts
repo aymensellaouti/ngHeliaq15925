1cafa83698c4e58cefea5b8a307a00b924f99835
aymensellaouti <aymen.sellaouti@gmail.com>
2025-09-15 14:30:58 +0100

    exemple optimisation computed

--- src/app/signals/som/som.component.ts
+++ src/app/signals/som/som.component.ts
@@ -10,5 +10,23 @@ import { FormsModule } from '@angular/forms';
 export class SomComponent {
   x = signal(3);
   y = signal(5);
-  z = computed(() => this.x() + this.y())
+  z = computed(() => this.x() + this.y());
+  plusGrandQue40 = computed(() => {
+    console.log('plusGrandQue 40 :D');
+    return this.z() > 40 ;
+  });
+
+  show = computed(() => {
+    console.log('show plus Grand Que 40');
+
+    if (this.plusGrandQue40()) {
+      return 'plus grand';
+    } else {
+      return 'plus petit';
+    }
+  })
+  doubleZ = computed(() => {
+    console.log('doubleZ :D');
+    return this.z() * 2;
+  });
 }
