a412d4d4cdc21d86d269c904d8860722ee2bb3da
aymensellaouti <aymen.sellaouti@gmail.com>
2025-09-16 11:14:55 +0100

    correction style

--- src/app/directives/mini-word/mini-word.component.ts
+++ src/app/directives/mini-word/mini-word.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-mini-word',
   templateUrl: './mini-word.component.html',
-  styleUrls: ['./mini-word.component.css']
+  styleUrls: ['./mini-word.component.css'],
+  imports: [FormsModule]
 })
 export class MiniWordComponent {
-
+  color = signal('black');
+  fontFamily = signal('garamond');
+  fontSize = signal(75);
 }
