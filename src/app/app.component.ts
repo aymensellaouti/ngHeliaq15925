1df97c18e305f171bff39b60daf309539998954c
aymensellaouti <aymen.sellaouti@gmail.com>
2025-09-17 08:58:17 +0100

    mise en place du routeur

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,17 @@
 import { Component, inject } from '@angular/core';
-import { FirstComponent } from "./components/first/first.component";
-import { SecondComponent } from "./components/second/second.component";
-import { ColorComponent } from "./components/color/color.component";
-import { TwoComponent } from "./components/two/two.component";
-import { RotatingCardComponent } from "./components/rotating-card/rotating-card.component";
-import { CounterComponent } from "./signals/counter/counter.component";
-import { SomComponent } from "./signals/som/som.component";
-import { TtcComponent } from "./signals/ttc/ttc.component";
-import { NamesComponent } from './signals/names/names.component';
-import { ShowIsEvenComponent } from "./interactionEntreComposant/show-is-even/show-is-even.component";
-import { PereComponent } from "./interactionEntreComposant/pere/pere.component";
-import { CvComponent } from "./cv/cv/cv.component";
-import { StyleComponent } from "./directives/style/style.component";
-import { MiniWordComponent } from "./directives/mini-word/mini-word.component";
-import { CurrencyPipe, DatePipe, UpperCasePipe } from '@angular/common';
-import { BtcToUsdPipe } from './pipes/btc-to-usd.pipe';
 import { FormsModule } from '@angular/forms';
 import { FirstService } from './services/first.service';
-import { TodoComponent } from "./todo/todo/todo.component";
-import { WeekTodoComponent } from "./todo/week-todo/week-todo.component";
 import { ToastrService } from 'ngx-toastr';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./components/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   imports: [
     FormsModule,
-    ColorComponent,
-    DatePipe,
-    UpperCasePipe,
-    BtcToUsdPipe,
-    CurrencyPipe,
-    CvComponent,
-    TodoComponent,
-    WeekTodoComponent,
-  ],
+    RouterOutlet,
+    NavbarComponent
+],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
